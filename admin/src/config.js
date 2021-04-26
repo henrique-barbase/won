e32aaf64f62872c2470ba88826912812372b4472
Henrique <henrique2029@hotmail.com>
2021-04-26 01:07:54 -0300

    first commit

---
+++ admin/src/config.js
@@ -0,0 +1,3 @@
+
+export const SHOW_TUTOTIALS = false;
+export const SETTINGS_BASE_URL = "/settings"
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '76afbc21-07fb-4cec-97c9-992c5ea3e6f2'
+};
